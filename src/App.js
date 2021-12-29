9e9f1be03e370cb41ce94c049d7f2e61994cb265
rahat <201853082050@nuist.edu.cn>
2021-12-29 12:35:16 +0600

    add dependencies react-redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="bg-gray-200 text-4xl p-4">welcome to preSetup react application</h1>
+      <h1 className="bg-gray-200 text-4xl p-4">
+        welcome to preSetup react application
+      </h1>
       <p className="p-3 text-xl">created by rahat</p>
+      <h2 className="underline font-bold text-blue-600">
+        installed dependencies
+      </h2>
+      <div className="text-red-700">
+        <h3>axios</h3>
+        <h3>react-router-dom</h3>
+        <h3>frontAwesome</h3>
+        <h3>tailwind css</h3>
+        <h3>react-redux</h3>
+      </div>
     </div>
   );
 }
